61ab8411315e53dfd2935abf523e148747eb64d0
Trevor Huis in 't Veld <trevorhuis@bithuis.com>
2022-09-25 17:33:55 -0500

    Add two api functions

---
+++ api/write.js
@@ -0,0 +1,32 @@
+const { Pool } = require('pg')
+
+let pool
+
+export default async function handler(request, response) {
+    if (!pool) {
+        const connectionString = process.env.DATABASE_URL
+        pool = new Pool({
+            connectionString,
+            max: 1
+        })
+    }
+
+    const resp = await insertText(pool, request.body.text)
+
+    response.status(200).json({
+      body: request.body,
+      query: request.query,
+      cookies: request.cookies,
+    });
+  }
+
+const insertText = async (p, text) => {
+    const client = await p.connect()
+    try {
+        await client.query('INSERT INTO sample (text) VALUES ($1)', [text])
+    } catch (err) {
+        console.log(err.stack)
+    } finally {
+        client.release()
+    }
+}
