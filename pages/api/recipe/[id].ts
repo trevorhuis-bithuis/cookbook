a2190892dc00c0bda806c06622a47da6a8de21d8
Trevor Huis in 't Veld <trevorhuis@bithuis.com>
2022-12-23 13:57:33 -0600

    Add single recipe page

--- pages/api/recipe/[id].ts
+++ pages/api/recipe/[id].ts
@@ -1,4 +1,3 @@
-import { Category, Ingredient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../lib/prisma'
 
@@ -6,6 +5,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const id: string = req.query.id as string
 
     const recipe = await prisma.recipe.findUnique({ where: { id }, include: { ingredients: true } })
+    console.log(recipe)
     res.status(200).json({ recipe })
 }
 
