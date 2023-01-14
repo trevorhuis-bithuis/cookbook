67ed0ac64b1b695fab284eb447a63f87bbdb88ec
Trevor Huis in 't Veld <trevorhuis@bithuis.com>
2023-01-14 13:05:42 -0600

    Do not load if session is unknown

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,15 @@
 import Header from "./header"
 import Footer from "./footer"
 import type { ReactNode } from "react"
+import { useSession } from "next-auth/react"
 
 export default function Layout({ children }: { children: ReactNode }) {
+    const { status } = useSession()
+
+    if (status === 'loading') {
+        return <div></div>
+    }
+
     return (
         <>
             <Header />
