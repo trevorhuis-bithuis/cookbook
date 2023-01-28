b4ce8ae30239a75eafefff05d34a7acc79f9a26c
Trevor Huis in 't Veld <trevorhuis@bithuis.com>
2023-01-27 23:24:55 -0600

    UI for single ingredient input

--- components/recipeForm/ingredientsInput.tsx
+++ components/recipeForm/ingredientsInput.tsx
@@ -53,48 +53,14 @@ export default function IngredientsInput(props: ingredientsInputProps) {
                         <div className="flex-1">
                             <div>
                                 <label htmlFor="ingredient" className="block text-sm font-medium text-gray-700">
-                                    Quantity
-                                </label>
-                                <div className="mt-1">
-                                    <input
-                                        type="number"
-                                        name="ingredient"
-                                        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                        placeholder="1.25"
-                                        onChange={(e) => { updateIngredientQuantity(index, parseFloat(e.target.value)) }}
-                                        value={ingredient.quantity}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="flex-1">
-                            <div>
-                                <label htmlFor="ingredient" className="block text-sm font-medium text-gray-700">
-                                    Unit
+                                    Ingredient {index + 1}
                                 </label>
                                 <div className="mt-1">
                                     <input
                                         type="text"
                                         name="ingredient"
                                         className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                        placeholder="Cups"
-                                        onChange={(e) => { updateIngredientUnit(index, e.target.value) }}
-                                        value={ingredient.unit}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="flex-1">
-                            <div>
-                                <label htmlFor="ingredient" className="block text-sm font-medium text-gray-700">
-                                    Ingredient
-                                </label>
-                                <div className="mt-1">
-                                    <input
-                                        type="text"
-                                        name="ingredient"
-                                        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                        placeholder="Butter"
+                                        placeholder="1/4 cup butter"
                                         onChange={(e) => { updateIngredientName(index, e.target.value) }}
                                         value={ingredient.name}
                                     />
@@ -102,7 +68,7 @@ export default function IngredientsInput(props: ingredientsInputProps) {
                             </div>
                         </div>
                         <div className="flex-none content-end">
-                            <button className="m-2" type="button" onClick={removeIngredient(index)}>
+                            <button className="mt-8" type="button" onClick={removeIngredient(index)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
