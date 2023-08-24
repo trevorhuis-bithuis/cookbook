5630a8a9b8f905d552c35ded98b6500b4e85e012
Trevor Huis in 't Veld <trevorhuis@bithuis.com>
2023-08-24 18:02:19 -0500

    fix sign out button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -110,16 +110,12 @@ export default function Header(props: HeaderProps) {
                       <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <Menu.Item>
                           {({ active }) => (
-                            <div
-                              className={classNames(
+                            <Form method="post" action="/logout">
+                              <button className={classNames(
                                 active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-gray-700",
-                              )}
-                            >
-                              <Form method="post" action="/logout">
-                                <button type="submit">Sign out</button>
-                              </Form>
-                            </div>
+                                "block w-full text-left px-4 py-2 text-sm text-gray-700",
+                              )} type="submit">Sign out</button>
+                            </Form>
                           )}
                         </Menu.Item>
                       </Menu.Items>
