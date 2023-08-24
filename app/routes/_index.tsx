e90df7f71f834652a1592b9dc1d688e0b3d6a423
Trevor Huis in 't Veld <trevorhuis@bithuis.com>
2023-08-24 10:54:35 -0500

    deploy remix

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+import { useOptionalUser } from "~/utils";
+
+export const meta: V2_MetaFunction = () => [{ title: "Crystal's Cookbook" }];
+
+export default function Index() {
+  const user = useOptionalUser();
+  return (
+    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+      <div className="relative sm:pb-16 sm:pt-8">
+        Crystal's Cookbook
+      </div>
+    </main>
+  );
+}
